758f01fc718c84d29916981bebe517ff24ce82d7
Alex Nguetcha <nguetchaalex@gmail.com>
2026-09-16 20:30:16 +0100

    feat: add Spring Boot backend skeleton

---
+++ backend/src/main/java/com/covataskmanager/CovaTaskManagerApplication.java
@@ -0,0 +1,12 @@
+package com.covataskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovaTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovaTaskManagerApplication.class, args);
+    }
+}
